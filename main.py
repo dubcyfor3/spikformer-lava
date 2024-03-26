e3a3b04668b9d00095df4cc226686dbd9e284acc
Chiyue Wei <1780277455@qq.com>
2024-03-26 00:05:13 -0400

    [update] load spikformer parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import typing as ty
 sys.path.append("~/lava/src")
 
@@ -37,7 +38,8 @@ class PyImageInputModel(PyLoihiProcessModel):
     image_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, float, precision=32)
 
     def run_spk(self):
-        image_out = torch.rand(self.image_out.shape)
+        dict = torch.load('input_data.pth')
+        image_out = dict['input_tensor'].cpu().numpy()
         self.image_out.send(image_out)
 
 class ClassOutput(AbstractProcess):
@@ -110,40 +112,73 @@ class PyMeanModel(PyLoihiProcessModel):
         tensor_out = torch.mean(tensor_in, dim=self.dim).numpy()
         self.tensor_out.send(tensor_out)
 
-def test_spikformer():
+def test_spikformer(checkpoint=None):
     timestep = 4
     batch_size = 8
-    image_size_h = 16
-    image_size_w = 16
+    image_size_h = 32
+    image_size_w = 32
     patch_size = 4
     in_channels = 3
-    embed_dims = 64 # 384
-    hidden_dim = 64*4 # 384*4
-    num_heads = 4 # 12
+    embed_dims = 384 # 384
+    hidden_dim = 384*4 # 384*4
+    num_heads = 12 # 12
     depth = 4
 
     # set seed for reproducibility
     np.random.seed(0)
     # weight for SPS
-    proj_conv_weight = np.random.rand(embed_dims//8, in_channels, 3, 3)
-    proj_bn_gamma = np.ones(embed_dims//8)
-    proj_bn_beta = np.zeros(embed_dims//8)
+    # proj_conv_weight = np.random.rand(embed_dims//8, in_channels, 3, 3)
+    # proj_bn_gamma = np.ones(embed_dims//8)
+    # proj_bn_beta = np.zeros(embed_dims//8)
 
-    proj1_conv_weight = np.random.rand(embed_dims//4, embed_dims//8, 3, 3)
-    proj1_bn_gamma = np.ones(embed_dims//4)
-    proj1_bn_beta = np.zeros(embed_dims//4)
+    proj_conv_weight = checkpoint['patch_embed.proj_conv.weight'].numpy()
+    proj_bn_gamma = checkpoint['patch_embed.proj_bn.weight'].numpy()
+    proj_bn_beta = checkpoint['patch_embed.proj_bn.bias'].numpy()
+    proj_bn_running_mean = checkpoint['patch_embed.proj_bn.running_mean'].numpy()
+    proj_bn_running_var = checkpoint['patch_embed.proj_bn.running_var'].numpy()
 
-    proj2_conv_weight = np.random.rand(embed_dims//2, embed_dims//4, 3, 3)
-    proj2_bn_gamma = np.ones(embed_dims//2)
-    proj2_bn_beta = np.zeros(embed_dims//2)
+    # proj1_conv_weight = np.random.rand(embed_dims//4, embed_dims//8, 3, 3)
+    # proj1_bn_gamma = np.ones(embed_dims//4)
+    # proj1_bn_beta = np.zeros(embed_dims//4)
 
-    proj3_conv_weight = np.random.rand(embed_dims, embed_dims//2, 3, 3)
-    proj3_bn_gamma = np.ones(embed_dims)
-    proj3_bn_beta = np.zeros(embed_dims)
 
-    rpe_conv_weight = np.random.rand(embed_dims, embed_dims, 3, 3)
-    rpe_bn_gamma = np.ones(embed_dims)
-    rpe_bn_beta = np.zeros(embed_dims)
+    proj1_conv_weight = checkpoint['patch_embed.proj_conv1.weight'].numpy()
+    proj1_bn_gamma = checkpoint['patch_embed.proj_bn1.weight'].numpy()
+    proj1_bn_beta = checkpoint['patch_embed.proj_bn1.bias'].numpy()
+    proj1_bn_running_mean = checkpoint['patch_embed.proj_bn1.running_mean'].numpy()
+    proj1_bn_running_var = checkpoint['patch_embed.proj_bn1.running_var'].numpy()
+
+
+    # proj2_conv_weight = np.random.rand(embed_dims//2, embed_dims//4, 3, 3)
+    # proj2_bn_gamma = np.ones(embed_dims//2)
+    # proj2_bn_beta = np.zeros(embed_dims//2)
+
+    proj2_conv_weight = checkpoint['patch_embed.proj_conv2.weight'].numpy()
+    proj2_bn_gamma = checkpoint['patch_embed.proj_bn2.weight'].numpy()
+    proj2_bn_beta = checkpoint['patch_embed.proj_bn2.bias'].numpy()
+    proj2_bn_running_mean = checkpoint['patch_embed.proj_bn2.running_mean'].numpy()
+    proj2_bn_running_var = checkpoint['patch_embed.proj_bn2.running_var'].numpy()
+
+    # proj3_conv_weight = np.random.rand(embed_dims, embed_dims//2, 3, 3)
+    # proj3_bn_gamma = np.ones(embed_dims)
+    # proj3_bn_beta = np.zeros(embed_dims)
+
+    proj3_conv_weight = checkpoint['patch_embed.proj_conv3.weight'].numpy()
+    proj3_bn_gamma = checkpoint['patch_embed.proj_bn3.weight'].numpy()
+    proj3_bn_beta = checkpoint['patch_embed.proj_bn3.bias'].numpy()
+    proj3_bn_running_mean = checkpoint['patch_embed.proj_bn3.running_mean'].numpy()
+    proj3_bn_running_var = checkpoint['patch_embed.proj_bn3.running_var'].numpy()
+
+    # rpe_conv_weight = np.random.rand(embed_dims, embed_dims, 3, 3)
+    # rpe_bn_gamma = np.ones(embed_dims)
+    # rpe_bn_beta = np.zeros(embed_dims)
+
+    rpe_conv_weight = checkpoint['patch_embed.rpe_conv.weight'].numpy()
+    rpe_bn_gamma = checkpoint['patch_embed.rpe_bn.weight'].numpy()
+    rpe_bn_beta = checkpoint['patch_embed.rpe_bn.bias'].numpy()
+    rpe_bn_running_mean = checkpoint['patch_embed.rpe_bn.running_mean'].numpy()
+    rpe_bn_running_var = checkpoint['patch_embed.rpe_bn.running_var'].numpy()
+
 
     SPS_block = SPS(shape=(batch_size,),
                 image_size_h=image_size_h,
@@ -154,21 +189,31 @@ def test_spikformer():
                 proj_conv_weight=proj_conv_weight,
                 proj_bn_gamma=proj_bn_gamma,
                 proj_bn_beta=proj_bn_beta,
+                proj_bn_running_mean=proj_bn_running_mean,
+                proj_bn_running_var=proj_bn_running_var,
                 proj1_conv_weight=proj1_conv_weight,
                 proj1_bn_gamma=proj1_bn_gamma,
                 proj1_bn_beta=proj1_bn_beta,
+                proj1_bn_running_mean=proj1_bn_running_mean,
+                proj1_bn_running_var=proj1_bn_running_var,
                 proj2_conv_weight=proj2_conv_weight,
                 proj2_bn_gamma=proj2_bn_gamma,
                 proj2_bn_beta=proj2_bn_beta,
+                proj2_bn_running_mean=proj2_bn_running_mean,
+                proj2_bn_running_var=proj2_bn_running_var,
                 proj3_conv_weight=proj3_conv_weight,
                 proj3_bn_gamma=proj3_bn_gamma,
                 proj3_bn_beta=proj3_bn_beta,
+                proj3_bn_running_mean=proj3_bn_running_mean,
+                proj3_bn_running_var=proj3_bn_running_var,
                 rpe_conv_weight=rpe_conv_weight,
                 rpe_bn_gamma=rpe_bn_gamma,
-                rpe_bn_beta=rpe_bn_beta)
+                rpe_bn_beta=rpe_bn_beta,
+                rpe_bn_running_mean=rpe_bn_running_mean,
+                rpe_bn_running_var=rpe_bn_running_var)
 
-    classifier_weight = np.random.rand(10, embed_dims)
-    classifier_bias = np.random.rand(10)
+    classifier_weight = checkpoint['head.weight'].numpy()
+    classifier_bias = checkpoint['head.bias'].numpy()
 
     class_output = ClassOutput(shape=(batch_size, embed_dims), num_time_steps=timestep, weight=classifier_weight, bias=classifier_bias)
     mean_output = Mean(shape=(batch_size, image_size_h*image_size_w//16, embed_dims), dim=1)
@@ -177,52 +222,64 @@ def test_spikformer():
     block_list_SSA = []
     block_list_MLP = []
     for i in range(depth):
-        weight = np.random.rand(embed_dims, embed_dims)
-        bias = np.random.rand(embed_dims)
+        # weight = np.random.rand(embed_dims, embed_dims)
+        # bias = np.random.rand(embed_dims)
 
-        gamma = np.ones(embed_dims)
-        beta = np.zeros(embed_dims)
+        # gamma = np.ones(embed_dims)
+        # beta = np.zeros(embed_dims)
 
         this_SSA = SSA(shape=(batch_size, image_size_h*image_size_w//16, embed_dims),
-                     weight_q_linear=weight,
-                     bias_q_linear=bias,
-                     weight_k_linear=weight,
-                     bias_k_linear=bias,
-                     weight_v_linear=weight,
-                     bias_v_linear=bias,
-                     weight_proj_linear=weight,
-                     bias_proj_linear=bias,
-                        beta_q_bn=beta,
-                        gamma_q_bn=gamma,
-                        beta_k_bn=beta,
-                        gamma_k_bn=gamma,
-                        beta_v_bn=beta,
-                        gamma_v_bn=gamma,
-                        beta_proj_bn=beta,
-                        gamma_proj_bn=gamma,
+                     weight_q_linear=checkpoint['block.{}.attn.q_linear.weight'.format(i)].numpy(),
+                     bias_q_linear=checkpoint['block.{}.attn.q_linear.bias'.format(i)].numpy(),
+                     weight_k_linear=checkpoint['block.{}.attn.k_linear.weight'.format(i)].numpy(),
+                     bias_k_linear=checkpoint['block.{}.attn.k_linear.bias'.format(i)].numpy(),
+                     weight_v_linear=checkpoint['block.{}.attn.v_linear.weight'.format(i)].numpy(),
+                     bias_v_linear=checkpoint['block.{}.attn.v_linear.bias'.format(i)].numpy(),
+                     weight_proj_linear=checkpoint['block.{}.attn.proj_linear.weight'.format(i)].numpy(),
+                     bias_proj_linear=checkpoint['block.{}.attn.proj_linear.bias'.format(i)].numpy(),
+                        beta_q_bn=checkpoint['block.{}.attn.q_bn.bias'.format(i)].numpy(),
+                        gamma_q_bn=checkpoint['block.{}.attn.q_bn.weight'.format(i)].numpy(),
+                        running_mean_q_bn=checkpoint['block.{}.attn.q_bn.running_mean'.format(i)].numpy(),
+                        running_var_q_bn=checkpoint['block.{}.attn.q_bn.running_var'.format(i)].numpy(),
+                        beta_k_bn=checkpoint['block.{}.attn.k_bn.bias'.format(i)].numpy(),
+                        gamma_k_bn=checkpoint['block.{}.attn.k_bn.weight'.format(i)].numpy(),
+                        running_mean_k_bn=checkpoint['block.{}.attn.k_bn.running_mean'.format(i)].numpy(),
+                        running_var_k_bn=checkpoint['block.{}.attn.k_bn.running_var'.format(i)].numpy(),
+                        beta_v_bn=checkpoint['block.{}.attn.v_bn.bias'.format(i)].numpy(),
+                        gamma_v_bn=checkpoint['block.{}.attn.v_bn.weight'.format(i)].numpy(),
+                        running_mean_v_bn=checkpoint['block.{}.attn.v_bn.running_mean'.format(i)].numpy(),
+                        running_var_v_bn=checkpoint['block.{}.attn.v_bn.running_var'.format(i)].numpy(),
+                        beta_proj_bn=checkpoint['block.{}.attn.proj_bn.bias'.format(i)].numpy(),
+                        gamma_proj_bn=checkpoint['block.{}.attn.proj_bn.weight'.format(i)].numpy(),
+                        running_mean_proj_bn=checkpoint['block.{}.attn.proj_bn.running_mean'.format(i)].numpy(),
+                        running_var_proj_bn=checkpoint['block.{}.attn.proj_bn.running_var'.format(i)].numpy(),
                      num_heads=num_heads)
 
         block_list_SSA.append(this_SSA)
 
-        fc1_linear_weight = np.random.rand(hidden_dim, embed_dims)
-        fc1_linear_bias = np.random.rand(hidden_dim)
-        fc2_linear_weight = np.random.rand(embed_dims, hidden_dim)
-        fc2_linear_bias = np.random.rand(embed_dims)
+        # fc1_linear_weight = np.random.rand(hidden_dim, embed_dims)
+        # fc1_linear_bias = np.random.rand(hidden_dim)
+        # fc2_linear_weight = np.random.rand(embed_dims, hidden_dim)
+        # fc2_linear_bias = np.random.rand(embed_dims)
 
-        fc1_bn_gamma = np.ones(hidden_dim)
-        fc1_bn_beta = np.zeros(hidden_dim)
-        fc2_bn_gamma = np.ones(embed_dims)
-        fc2_bn_beta = np.zeros(embed_dims)
+        # fc1_bn_gamma = np.ones(hidden_dim)
+        # fc1_bn_beta = np.zeros(hidden_dim)
+        # fc2_bn_gamma = np.ones(embed_dims)
+        # fc2_bn_beta = np.zeros(embed_dims)
 
         this_MLP = SpikingMLP(shape=(batch_size, image_size_h*image_size_w//16, embed_dims, hidden_dim, embed_dims),
-                 fc1_linear_weight=fc1_linear_weight,
-                 fc1_linear_bias=fc1_linear_bias,
-                 fc2_linear_weight=fc2_linear_weight,
-                 fc2_linear_bias=fc2_linear_bias,
-                 fc1_bn_gamma=fc1_bn_gamma,
-                 fc1_bn_beta=fc1_bn_beta,
-                 fc2_bn_gamma=fc2_bn_gamma,
-                 fc2_bn_beta=fc2_bn_beta)
+                 fc1_linear_weight=checkpoint['block.{}.mlp.fc1_linear.weight'.format(i)].numpy(),
+                 fc1_linear_bias=checkpoint['block.{}.mlp.fc1_linear.bias'.format(i)].numpy(),
+                 fc2_linear_weight=checkpoint['block.{}.mlp.fc2_linear.weight'.format(i)].numpy(),
+                 fc2_linear_bias=checkpoint['block.{}.mlp.fc2_linear.bias'.format(i)].numpy(),
+                 fc1_bn_gamma=checkpoint['block.{}.mlp.fc1_bn.weight'.format(i)].numpy(),
+                 fc1_bn_beta=checkpoint['block.{}.mlp.fc1_bn.bias'.format(i)].numpy(),
+                 fc1_bn_running_mean=checkpoint['block.{}.mlp.fc1_bn.running_mean'.format(i)].numpy(),
+                 fc1_bn_running_var=checkpoint['block.{}.mlp.fc1_bn.running_var'.format(i)].numpy(),
+                 fc2_bn_gamma=checkpoint['block.{}.mlp.fc2_bn.weight'.format(i)].numpy(),
+                 fc2_bn_beta=checkpoint['block.{}.mlp.fc2_bn.bias'.format(i)].numpy(),
+                 fc2_bn_running_mean=checkpoint['block.{}.mlp.fc2_bn.running_mean'.format(i)].numpy(),
+                 fc2_bn_running_var=checkpoint['block.{}.mlp.fc2_bn.running_var'.format(i)].numpy())
 
         block_list_MLP.append(this_MLP)
 
@@ -246,5 +303,14 @@ def test_spikformer():
         print('classfy result: ', class_output.classify_result.get())
         print('\n ----- \n')
 
+    SPS_block.stop()
+
 if __name__ == '__main__':
-    test_spikformer()
+    checkpoint_path = 'model_best.pth.tar'
+    if os.path.isfile(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path, map_location='cpu')
+        if isinstance(checkpoint, dict) and 'state_dict' in checkpoint:
+            checkpoint = checkpoint['state_dict']
+            # print all keys
+            # print(checkpoint.keys())
+    test_spikformer(checkpoint=checkpoint)
